a4bb13f20b4ff9e3bc978df82b9beab8b1b1eda7
khaelys <miragliotta.giuseppe7@gmail.com>
2021-09-01 16:45:07 +0200

    feat: Externalize EntryFillDialog strings

--- src/Clockify/ClockifyMessageSource.cs
+++ src/Clockify/ClockifyMessageSource.cs
@@ -12,8 +12,20 @@ namespace Bot.Clockify
         }
 
         public string SetupRequest => GetString(nameof(SetupRequest));
-        public string SetupFeedback => GetString(nameof(SetupRequest));
+        public string SetupFeedback => GetString(nameof(SetupFeedback));
         public string SetupReject => GetString(nameof(SetupReject));
+        public string TaskCreationError => GetString(nameof(TaskCreationError));
+        public string TaskUnrecognized => GetString(nameof(TaskUnrecognized));
+        public string TaskCreation => GetString(nameof(TaskCreation));
+        public string TaskAbort => GetString(nameof(TaskAbort));
+        public string AddEntryFeedback => GetString(nameof(AddEntryFeedback));
+        public string EntryFillUnderstandingError => GetString(nameof(EntryFillUnderstandingError));
+        public string AmbiguousProjectError => GetString(nameof(AmbiguousProjectError));
+        public string ProjectUnrecognized => GetString(nameof(ProjectUnrecognized));
+        public string TaskUnrecognizedRetry => GetString(nameof(TaskUnrecognizedRetry));
+        public string TaskSelectionQuestion => GetString(nameof(TaskSelectionQuestion));
+        public string NewTask => GetString(nameof(NewTask));
+        public string No => GetString(nameof(No));
 
         private string GetString(string name) => _localizer[name];
     }
--- src/Clockify/IClockifyMessageSource.cs
+++ src/Clockify/IClockifyMessageSource.cs
@@ -1,10 +1,26 @@
-﻿namespace Bot.Clockify
+﻿using System;
+
+namespace Bot.Clockify
 {
     public interface IClockifyMessageSource
     {
         string SetupRequest { get; }
         string SetupFeedback { get; }
         string SetupReject { get; }
+        
+        string TaskCreationError { get; }
+        
+        string TaskUnrecognized { get; }
+        string TaskCreation { get; }
+        string TaskAbort { get; }
+        string AddEntryFeedback { get; }
+        string EntryFillUnderstandingError { get; }
+        string AmbiguousProjectError { get; }
+        string ProjectUnrecognized { get; }
+        string TaskUnrecognizedRetry { get; }
+        string TaskSelectionQuestion { get; }
+        string NewTask { get; }
+        string No { get; }
     }
     
 }
