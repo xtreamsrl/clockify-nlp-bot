dce55c015796d81a05fe8745a0a4a6ddef380138
khaelys <miragliotta.giuseppe7@gmail.com>
2021-09-09 12:54:25 +0200

    test: Convert ClockifySetupDialogTest to unit test

--- tests/Bot.Tests/Clockify/Dialogs/ClockifySetupDialogTest.cs
+++ tests/Bot.Tests/Clockify/Dialogs/ClockifySetupDialogTest.cs
@@ -1,47 +1,54 @@
 ﻿using System;
 using Bot.Clockify;
 using Bot.Clockify.Client;
+using Bot.Clockify.Models;
 using Bot.Data;
 using Bot.States;
 using FluentAssertions;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Testing;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
 using Moq;
 using Xunit;
-using static Bot.Integration.Tests.Clockify.ClockifyConsts;
 
-namespace Bot.Integration.Tests.Dialogs
+namespace Bot.Tests.Clockify.Dialogs
 {
-    public class UserSetupDialogTest
+    public class ClockifySetupDialogTest
     {
+        private const string ClockifyApiKey = nameof(ClockifyApiKey);
         private const string SetupRequest = "SetupRequest";
         private const string SetupReject = "SetupReject";
         private const string SetupFeedback = "SetupFeedback";
-        
+
         [Fact]
-        private async void UserSetupDialogMainFlowTest()
+        private async void ClockifySetupDialog_UserProvideValidToken_GiveFeedbackToUserAndSaveTokenId()
         {
             var userState = new UserState(new MemoryStorage());
-            var clockifyService = new ClockifyService(new ClockifyClientFactory());
+            var mockClockifyService = new Mock<IClockifyService>();
+            mockClockifyService.Setup(c => c.GetCurrentUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(new UserDo
+                {
+                    Id = "1234",
+                    Name = "John Doe",
+                    Email = "johndoe@gmail.com",
+                    ActiveWorkspace = "w1",
+                    DefaultWorkspace = "w1"
+                });
             var clockifyMessageSource = new Mock<IClockifyMessageSource>();
             clockifyMessageSource.Setup(c => c.SetupRequest).Returns(SetupRequest);
-            clockifyMessageSource.Setup(c => c.SetupReject).Returns(SetupReject);
             clockifyMessageSource.Setup(c => c.SetupFeedback).Returns(SetupFeedback);
             var mockTokenRepository = new Mock<ITokenRepository>();
             mockTokenRepository.Setup(r => r.WriteAsync(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(new TokenData("id", ClockifyApiKey));
 
-            var dialog = new ClockifySetupDialog(userState, clockifyService, mockTokenRepository.Object,
+            var dialog = new ClockifySetupDialog(userState, mockClockifyService.Object, mockTokenRepository.Object,
                 clockifyMessageSource.Object);
             var dialogTestClient = new DialogTestClient(Channels.Telegram, dialog);
 
             var reply = await dialogTestClient.SendActivityAsync<IMessageActivity>("ciao");
             reply.Text.Should().Be(SetupRequest);
-
-            reply = await dialogTestClient.SendActivityAsync<IMessageActivity>(InvalidApiKey);
-            reply.Text.Should().Be(SetupReject);
 
             reply = await dialogTestClient.SendActivityAsync<IMessageActivity>(ClockifyApiKey);
             reply.Text.Should().Be(SetupFeedback);
@@ -50,12 +57,11 @@ namespace Bot.Integration.Tests.Dialogs
                 .GetAsync(dialogTestClient.DialogContext.Context);
 
             userProfile.ClockifyTokenId.Should().NotBeNull();
-
-            // TODO: How do we test the end of the dialog?
+            dialogTestClient.DialogTurnResult.Status.Should().Be(DialogTurnStatus.Complete);
         }
 
         [Fact]
-        private async void UserSetupDialog_ClockifyThrows401_ShouldAskAgain()
+        private async void ClockifySetupDialog_ClockifyThrows401_ShouldAskAgain()
         {
             var userState = new UserState(new MemoryStorage());
             var clockifyService = new Mock<IClockifyService>();
