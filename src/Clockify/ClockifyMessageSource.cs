eeeb0feff13d6fba40b397deb7bec7d64a22dbd1
simonecolucci93 <simone.colucci@xtreamers.io>
2021-09-15 10:55:30 +0200

    feat: pick random Clockify messages when there are options

--- src/Clockify/ClockifyMessageSource.cs
+++ src/Clockify/ClockifyMessageSource.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Extensions.Localization;
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Localization;
 
 namespace Bot.Clockify
 {
     public class ClockifyMessageSource : IClockifyMessageSource
     {
+        private static Random rnd = new Random();
+        
         private readonly IStringLocalizer<ClockifyMessageSource> _localizer;
 
         public ClockifyMessageSource(IStringLocalizer<ClockifyMessageSource> localizer)
@@ -14,7 +18,7 @@ namespace Bot.Clockify
         public string SetupRequest => GetString(nameof(SetupRequest));
         public string SetupFeedback => GetString(nameof(SetupFeedback));
         public string SetupReject => GetString(nameof(SetupReject));
-        
+
         public string TaskCreationError => GetString(nameof(TaskCreationError));
         public string TaskUnrecognized => GetString(nameof(TaskUnrecognized));
         public string TaskCreation => GetString(nameof(TaskCreation));
@@ -27,18 +31,27 @@ namespace Bot.Clockify
         public string TaskSelectionQuestion => GetString(nameof(TaskSelectionQuestion));
         public string NewTask => GetString(nameof(NewTask));
         public string No => GetString(nameof(No));
-        
+
         public string ReportDateRangeError => GetString(nameof(ReportDateRangeError));
         public string ReportWorkedPeriodUnrecognized => GetString(nameof(ReportWorkedPeriodUnrecognized));
         public string ReportTotalHours => GetString(nameof(ReportTotalHours));
         public string ReportNoWork => GetString(nameof(ReportNoWork));
         public string ReportWork => GetString(nameof(ReportWork));
         public string ReportDateRangeExceedOneYear => GetString(nameof(ReportDateRangeExceedOneYear));
-        
+
         public string RemindStoppedAlready => GetString(nameof(RemindStoppedAlready));
         public string RemindStopAnswer => GetString(nameof(RemindStopAnswer));
+
         public string RemindEntryFill => GetString(nameof(RemindEntryFill));
 
-        private string GetString(string name) => _localizer[name];
+        private string GetString(string name)
+        {
+            string? key = _localizer.GetAllStrings()
+                .Select(s => s.Name)
+                .Where(k => k.StartsWith(name))
+                .OrderBy(_ => rnd.Next())
+                .First();
+            return _localizer[key];
+        }
     }
 }
