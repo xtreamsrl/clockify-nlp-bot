c60a0d8b5f82e62d1cc93f924a205e898f076445
Kashif Soofi <kashif.soofi+git@gmail.com>
2021-10-06 21:21:07 +0100

    Use InMemoryCache in TokenRepository

--- src/Data/TokenRepository.cs
+++ src/Data/TokenRepository.cs
@@ -2,28 +2,36 @@
 using System.Threading.Tasks;
 using Azure;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Bot.Data
 {
     internal class TokenRepository : ITokenRepository
     {
+        private IMemoryCache _cache;
         private readonly SecretClient _secretClient;
         
         // TODO add caching
 
-        public TokenRepository(SecretClient secretClient)
+        public TokenRepository(IMemoryCache cache, SecretClient secretClient)
         {
+            _cache = cache;
             _secretClient = secretClient;
         }
 
         public async Task<TokenData> ReadAsync(string id)
         {
             if (id == null) throw new ArgumentNullException(id);
-            
+
+            if (_cache.TryGetValue<TokenData>(id, out var cachedTokenData))
+            {
+                return cachedTokenData;
+            }
+
             try
             {
                 KeyVaultSecret secret = await _secretClient.GetSecretAsync(id);
-                return new TokenData(secret.Name, secret.Value);
+                return GetTokenData(secret);
             }
             catch (RequestFailedException e)
             {
@@ -42,7 +50,14 @@ namespace Bot.Data
             
             string name = id ?? Guid.NewGuid().ToString();
             KeyVaultSecret secret = await _secretClient.SetSecretAsync(name, value);
-            return new TokenData(secret.Name, secret.Value);
+            return GetTokenData(secret);
+        }
+
+        private TokenData GetTokenData(KeyVaultSecret secret)
+        {
+            var tokenData = new TokenData(secret.Name, secret.Value);
+            _cache.Set(tokenData.Id, tokenData);
+            return tokenData;
         }
     }
 }
