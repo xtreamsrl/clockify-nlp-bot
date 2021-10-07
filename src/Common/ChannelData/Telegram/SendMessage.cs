3b219f20f70b00aecc9e68b2cd9d3e26f438ad29
Riccardo Zoncada <riccardo.zoncada@xtreamers.io>
2021-10-07 16:36:00 +0200

    feat: add ChannelData models for sendMessage Telegram API method

---
+++ src/Common/ChannelData/Telegram/SendMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Bot.Common.ChannelData.Telegram
+{
+    public class SendMessage: ChannelData<SendMessageParameters>
+    {
+        private static string Method => "sendMessage";
+        
+        public SendMessage(SendMessageParameters parameters) : base(Method, parameters)
+        {
+        }
+    }
+}
