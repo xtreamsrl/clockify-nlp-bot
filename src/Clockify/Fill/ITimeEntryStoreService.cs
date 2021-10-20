096fd3801eda7ea1c0e776b2a0ce80531b248274
Simone Colucci <simone3991@users.noreply.github.com>
2021-10-21 00:08:23 +0200

    feat: Allow for worked period computation (#101)
    * feat: allow for worked period to be used
    
    * merge: merge from master
    
    * test: update test case accordingly
    
    * refactor: extract variable
    
    Co-authored-by: simonecolucci93 <simone.colucci@xtreamers.io>
    Co-authored-by: Giuseppe Miragliotta <miragliotta.giuseppe7@gmail.com>
    Co-authored-by: Riccardo Zoncada <zoncrd@gmail.com>

--- src/Clockify/Fill/ITimeEntryStoreService.cs
+++ src/Clockify/Fill/ITimeEntryStoreService.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Bot.Clockify.Models;
 
 namespace Bot.Clockify.Fill
 {
     public interface ITimeEntryStoreService
     {
-        public Task<double> AddTimeEntries(string clockifyToken, ProjectDo project, TaskDo? task, double minutes);
+        public Task<double> AddTimeEntries(string clockifyToken, ProjectDo project, TaskDo? task, DateTime start, DateTime end);
     }
 }
